84b6e9b7e587a4bf2fbff8b3adcdd5c2f164c3c9
jmsciabica <jessie.wrigley@gmail.com>
2014-10-14 10:53:54 -0500

    Epsilon Practice Lab

---
+++ Epsilon/Epsilon.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Epsilon
+{
+    public static void main( String [] args)
+    {
+       final double EPSILON = 1e-14;
+       Scanner in = new Scanner(System.in);
+       
+       System.out.print("Enter a floating- point number: ");
+       double value = in.nextDouble();
+       
+       String magnitude = "";
+       if (Math.abs(value) < 1.0)
+       {
+           magnitude = "small"; 
+       }
+       else if (Math.abs(value) > 1000000.0)
+       {
+           magnitude = "small"; 
+       }
+       
+       if (Math.abs(value - 0)< EPSILON)
+       {
+           System.out.println("Valuie is zero.");
+       }
+       else if(value > 0)
+       {
+           System.out.println("Value is a " + magnitude + "postitive number.");
+       }
+       else
+       {
+          System.out.println("Value is a " + magnitude + "negative number.");
+       }
+       }
+}
+
